10ba06a26f07d403dddcc60a06802c60fb204bce
roguelich <34791211+roguelich@users.noreply.github.com>
2018-02-27 15:30:40 +0100

    Add files via upload

---
+++ LeonCog.py
@@ -0,0 +1,245 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 25 21:53:00 2018
+
+@author: Lenovo
+"""
+import discord
+from discord.ext import commands
+import colorsys
+import random
+            
+spectrums = """```
+red-yellow
+yellow-green
+green-cyan
+cyan-blue
+blue-magenta
+magenta-red
+```"""
+def random_color():
+    x = lambda: random.randint(0, 255)
+    return discord.Colour.from_rgb(x(), x(), x())
+
+def random_spec(spec):
+    hue = lambda start, end: random.uniform(start, end)
+    x = random.random
+    if spec == "red-yellow":
+        h = hue(0, 1/6)
+    elif spec == "yellow-green":
+        h = hue(1/6, 1/3)
+    elif spec == "green-cyan":
+        h = hue(1/3, 1/2)
+    elif spec == "cyan-blue":
+        h = hue(1/2, 2/3)
+    elif spec == "blue-magenta":
+        h = hue(2/3, 5/6)
+    elif spec == "magenta-red":
+        h = hue(5/6, 1)
+    else: 
+        return
+    r,g,b = colorsys.hls_to_rgb(h, x(), x())
+    convert = lambda x: int(x*255)
+    r,g,b = convert(r), convert(g), convert(b)
+    return discord.Colour.from_rgb(r,g,b)
+
+def check_if_not_following(ctx):
+    author = ctx.author
+    leon_role = discord.utils.find(lambda role: role.name.startswith("leon "), author.roles)
+    if leon_role is None or author.name in leon_role.name:
+        return True
+    elif ctx.invoked_with == "follow":
+        return True
+    return False
+
+def check_if_following(ctx):
+    author = ctx.author
+    leon_role = discord.utils.find(lambda role: role.name.startswith("leon "), author.roles)
+    if leon_role:
+        if author.name not in leon_role.name:
+            return True
+    return False
+
+class LeonCog:
+    def __init__(self, bot):
+        self.bot = bot
+        self.data = {}
+        
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.bot.user.name)
+        print(self.bot.user.id)
+        print('------')
+        self.data = await self.bot.load_data()
+        
+    @commands.command(name='help')
+    async def _help(self, ctx):
+        embed = discord.Embed(title="LeonBot", colour=random_color(), description="A list of LeonBot Commands")
+        embed.set_thumbnail(url=ctx.guild.icon_url)
+        embed.add_field(name="help", value="Shows this message.")
+        embed.add_field(name="rand [spectrum]", value="Get either a completely random color or that within declared hue spectrum")
+        embed.add_field(name="rainbow [spectrum]", value="Enable random color every post. You can also declare a specific hue spectrum!")
+        embed.add_field(name="rainbow off", value="Turn off the rainbow")
+        embed.add_field(name="spec", value="Show available hue spectrums", inline=False)
+        embed.add_field(name="get [member]", value="Get your or another member's colour")
+        embed.add_field(name="camouflage (member)", value="Get someone else's color!", inline=False)
+        embed.add_field(name="follow (member)", value="Follow somoeone's color. While you're following someone, you cannot invoke any commands except follow and unfollow.")
+        embed.add_field(name="unfollow", value="Stop following another member")
+        await ctx.send(content=None, embed=embed)
+        
+    @commands.command()
+    async def spec(self, ctx):
+        await ctx.send(spectrums)
+        
+    @commands.command()
+    async def get(self, ctx, other_member=None):
+        guild = ctx.guild
+        if other_member is None:
+            author = ctx.author
+            role = discord.utils.get(author.roles, name="leon " + author.name)
+            color = role.color
+            await ctx.send("Your color is: `{}`." .format(str(color)))
+        else:
+            member_found = discord.utils.get(guild.members, name=other_member)
+            if member_found is None:
+                await ctx.send("{} not found!" .format(other_member))
+                return
+            members_role = discord.utils.get(member_found.roles, name="leon " + other_member)
+            if members_role:
+                await ctx.send("{}'s color is `{}`." .format(other_member, str(members_role.color)))
+            if members_role is None:
+                await ctx.send("{} doesn't have a valid role!" .format(other_member))
+
+    @commands.command()
+    @commands.check(check_if_not_following)
+    async def rand(self, ctx, spec=None):
+        guild = ctx.guild
+        author = ctx.author
+        role_name = "leon " + ctx.author.name
+        has_role = discord.utils.get(author.roles, name=role_name)
+        if spec is None and not has_role:
+            role = await guild.create_role(name=role_name, color=random_color())
+            await author.add_roles(role)
+        elif spec and not has_role:
+            role = await guild.create_role(name=role_name, color=random_spec(spec))
+            await author.add_roles(role)
+        elif spec is None and has_role:
+            await has_role.edit(color=random_color())
+        elif spec and has_role:
+            await has_role.edit(color=random_spec(spec))
+            
+    @commands.group()
+    @commands.check(check_if_not_following)
+    async def rainbow(self, ctx, spec=None):
+        if ctx.invoked_subcommand is None:
+            guild = ctx.guild
+            guild.id = str(guild.id)
+            author = ctx.author
+            if spec is None:
+                self.data[guild.id][author.name] = {"rainbow":True, "spec":None}
+            else: 
+                self.data[guild.id][author.name] = {"rainbow":True, "spec":spec}
+            
+    @rainbow.command()
+    async def off(self, ctx, description="Turn the rainbow off, retaining your last color."):
+        guild = ctx.guild
+        guild.id = str(guild.id)
+        author = ctx.author
+        self.data[guild.id][author.name] = {"rainbow":False, "spec":None}
+        
+    @commands.command(name='set')
+    @commands.check(check_if_not_following)
+    async def setcolor(self, ctx, color, description="Set your own color."):
+        color = color[1:]
+        color = int(color, 16)
+        color = discord.Colour(color)
+        guild = ctx.guild
+        author = ctx.author
+        role_name = "leon " + author.name
+        has_role = discord.utils.get(author.roles, name=role_name)
+        if has_role:
+            await has_role.edit(color=color)
+        else:
+            role = await guild.create_role(name=role_name, color=color)
+            await author.add_roles(role)
+        
+    @commands.command()
+    @commands.check(check_if_not_following)
+    async def camouflage(self, ctx, other_member):
+        guild = ctx.guild
+        member_found = discord.utils.get(guild.members, name=other_member)
+        if member_found is None:
+            await ctx.send("{} not found!" .format(other_member))
+        members_role = discord.utils.get(member_found.roles, name="leon " + other_member)
+        if members_role:
+            color = members_role.colour
+            await ctx.invoke(self.setcolor, str(color))
+        elif members_role is None:
+            await ctx.send("{} doesn't have a valid role!" .format(other_member))
+            
+    @commands.command()
+    @commands.check(check_if_not_following)
+    async def follow(self, ctx, other_member):
+        author = ctx.author
+        guild = ctx.guild
+        member_found = discord.utils.get(guild.members, name=other_member)
+        if member_found is None:
+            await ctx.send("{} not found!" .format(other_member))
+            return
+        members_role = discord.utils.get(member_found.roles, name = "leon " + other_member)
+        if members_role:
+            await author.add_roles(members_role)
+            role_name = "leon " + author.name
+            has_role = discord.utils.get(author.roles, name=role_name)
+            if has_role:
+                await has_role.delete()
+            is_following = discord.utils.find(lambda role: role.name.startswith("leon "), author.roles)
+            if is_following:
+                await ctx.author.remove_roles(is_following)
+        elif members_role is None:
+            await ctx.send("{} doesn't have a valid role!" .format(other_member))
+            
+    @commands.command()
+    @commands.check(check_if_following)
+    async def unfollow(self, ctx):
+        author = ctx.author
+        role = discord.utils.find(lambda role: role.name.startswith("leon "), author.roles)
+        await author.remove_roles(role)
+            
+    async def on_message(self, message):
+        ctx = await self.bot.get_context(message)
+        if ctx.valid:
+            #await self.bot.process_commands(message)
+            return
+        author = message.author
+        guild = author.guild
+        guild.id = str(guild.id)
+        if author.name not in self.data[guild.id]:
+            return
+        rainbow = self.data[guild.id][author.name]["rainbow"]
+        not_following = check_if_not_following(ctx)
+        if rainbow and not_following:
+            spec = self.data[guild.id][author.name]["spec"]
+            if spec:
+                await ctx.invoke(self.rand, spec=spec)
+            else: 
+                await ctx.invoke(self.rand)
+        
+    @commands.command()
+    async def _save(self, ctx):
+        self.bot.save_data()
+                
+    async def on_command_error(self, ctx, error):
+        inv = ctx.invoked_with
+        if inv == "rand" or inv == "rainbow" or inv == "rainbow off" or \
+        inv == "setcolor" or inv == "camouflage":
+            if isinstance(type(error), type(commands.errors.CheckFailure)):
+                await ctx.send("Error! You're following someone!")
+        elif inv == "unfollow":
+            if isinstance(type(error), type(commands.errors.CheckFailure)):
+                await ctx.send("Error! You aren't following anyone yet!")
+        else:
+            print(error)
+            
+def setup(bot):
+    bot.add_cog(LeonCog(bot))
--- leon.py
+++ leon.py
@@ -1,176 +1,43 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec 21 12:59:05 2017
+Created on Sun Feb 25 13:44:06 2018
 
 @author: Lenovo
 """
-import discord
-import random
+from discord.ext import commands
 import asyncio
-import os
+import json
 
-client = discord.Client()
+#bot = commands.Bot(command_prefix ='!leon ')
 
-help_info = """`!leon` - activate/deactivate random colours every post
-`!leon keep` - keep your current colour
-`!leon set [#000000]` - set your colour to #000000
-`!leon random` - set your colour to random one
-`!leon camouflage [member]` - set your colour to that of the other member
-`!leon get` - get your colour"""
-
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-def randomColor():
-    r = lambda: random.randint(0,255)
-    color = '%02X%02X%02X' % (r(),r(),r())
-    color = color.lower()
-    return discord.Colour(int(color, 16))
-
-async def getRole(author, role_start):
-    roles = author.roles
-    for role in roles:
-        role_name = role.name
-        if role_name.startswith(role_start):
-            return role
-    return False
-
-async def isColor(server, color):
-    for role in server.roles:
-        role_name = role.name
-        if role_name.startswith("p_leon"):
-            if role.color == color:
-                return role
-    return False
-
-async def leon(author, call=None):
-    server = author.server
-    is_author = await getRole(author, "leon")
-    if is_author != False and call != None:
-        await client.delete_role(server, is_author)
-    elif is_author == False and call != None:
-        role_name = author.name
-        role_name = "leon " + role_name
-        new_role = await client.create_role(server, name=role_name, colour=randomColor())
-        await client.add_roles(author, new_role)
-        check = await getRole(author, "p_leon")
-        if check != None:
-            try:
-                await client.remove_roles(author, check)
-            except:
-                pass
-    else:
-        if is_author != False:
-            await client.edit_role(server, role=is_author, colour=randomColor())
-
-async def clean(server):
-    roles = server.roles
-    members = server.members
-    unused = []
-    counter = 0
-    for role in roles:
-        used = False
-        for member in members:
-            if role in member.roles:
-                used = True
-                break
-        if used == False:
-            unused.append(role)
-    for role in unused:
-        try:
-            await client.delete_role(server, role)
-            counter += 1
-        except:
-            pass
-    return print("{} roles deleted.".format(counter))
-
-async def persistent_leon(author):
-    server = author.server
-    role = await getRole(author, "leon")
-    role_color = role.color
-    color_str = str(role_color)
-    role_name = "p_leon " + color_str
-    check = discord.utils.get(server.roles, name=role_name)
-    if check != None:
-        await client.add_roles(author, check)
-    else:
-        new_role = await client.create_role(server, name=role_name, colour=role_color)
-        await client.add_roles(author, new_role)
-    await client.delete_role(server, role)
-
-async def new_persistent_leon(author, color):
-    server = author.server
-    check_leon = await getRole(author, "leon")
-    if check_leon != False:
-        await client.delete_role(server, check_leon)
-    check_p_leon = await getRole(author, "p_leon")
-    if check_p_leon != None:
-        try:
-            await client.remove_roles(author, check_p_leon)
-        except:
-            pass
-    check = await isColor(server, color)
-    if check != False:
-        await client.add_roles(author, check)
-    else:
-        role_name = "p_leon " + str(color)
-        new_role = await client.create_role(server, name=role_name, colour=color)
-        await client.add_roles(author, new_role)
-
-async def clean_roles():
-    await client.wait_until_ready()
-    server = client.get_server(id="183926953365995520")
-    await asyncio.sleep(5)
-    while not client.is_closed:
-        print("Cleaning roles...")
-        await clean(server)
+class Leon(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.bg_task = self.loop.create_task(self.save_data())
+        
+    async def load_data(self):
+        try: 
+            with open("data.txt", "r") as f:
+                self.data = json.load(f)
+            print("Data for {} succesfully loaded" .format([guild.name for guild in self.guilds]))
+            return self.data
+        except Exception as e:
+            print(e)
+            self.data = {}
+            for guild in self.guilds:
+                self.data[guild.id] = {}
+            print("Data for {} succesfully created" .format([guild.name for guild in self.guilds]))
+            return self.data
+    
+    async def save_data(self):
+        await self.wait_until_ready()
+        await asyncio.sleep(5)
+        with open("data.txt", "w") as f:
+            await json.dump(self.data, f)
         await asyncio.sleep(600)
 
-@client.event
-async def on_message(message):
-    author = message.author
-    server = author.server
-    if author == client.user:
-        return
-    elif message.content.startswith("!leon help"):
-        await client.send_message(message.channel, help_info)
-    elif message.content == "!leon":
-        await leon(author, call=True)
-    elif message.content == "!leon keep":
-        await persistent_leon(author)
-    elif message.content == "!leon clean":
-        await clean(server)
-    elif message.content == "!leon set #000000":
-        await client.send_message(message.channel, "Black is not a colour!")
-    elif message.content.startswith("!leon set"):
-        color_hex = message.content
-        color_hex = color_hex.split()
-        color_hex = color_hex[-1]
-        color_hex = color_hex[1:]
-        new_color = discord.Colour(int(color_hex, 16))
-        await new_persistent_leon(author, new_color)
-    elif message.content.startswith("!leon random"):
-        color = randomColor()
-        await new_persistent_leon(author, color)
-    elif message.content.startswith("!leon get"):
-        await client.send_message(message.channel, content="Your colour is: {}" .format(author.color))
-    elif message.content.startswith("!leon camouflage"):
-        other_member = message.content
-        other_member = other_member.split()
-        other_member = other_member[-1]
-        member = discord.utils.find(lambda m: m.name == '{}'.format(other_member), server.members)
-        try:
-            color = member.color
-            await new_persistent_leon(author, color)
-        except:
-            print(member)
-    else:
-        await leon(author)
 
-client.loop.create_task(clean_roles())
-token = os.environ.get('TOKEN')
-client.run(token)
+bot = Leon(command_prefix ='!leon ')
+bot.remove_command('help')
+bot.load_extension('LeonCog') 
+bot.run('MzkzMzcxMTk2NDYyMjAyODgw.DSZ26Q.lZCbbLEFEQNHvbFW5J5PuIej-6U')
